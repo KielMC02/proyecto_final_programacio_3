e05e98a9eaf5e807f9138160f7439a30b3f6515b
KielMC02 <49813048+KielMC02@users.noreply.github.com>
2019-11-26 09:50:51 -0400

    *Se realizo Conexion a la Base de Datos.

---
+++ HUMAN_RESOURCES_v1/HUMAN_RESOURCES_v1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HUMAN_RESOURCES_v1.Startup))]
+namespace HUMAN_RESOURCES_v1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

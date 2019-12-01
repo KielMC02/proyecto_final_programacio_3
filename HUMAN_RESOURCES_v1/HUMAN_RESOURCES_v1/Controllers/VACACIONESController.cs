5ba02bf57b79324d6d5cb32444827e7f42af0f6f
KielMC02 <49813048+KielMC02@users.noreply.github.com>
2019-12-01 11:43:43 -0400

    Sicronizacion de cambios.

--- HUMAN_RESOURCES_v1/HUMAN_RESOURCES_v1/Controllers/VACACIONESController.cs
+++ HUMAN_RESOURCES_v1/HUMAN_RESOURCES_v1/Controllers/VACACIONESController.cs
@@ -10,18 +10,18 @@ using HUMAN_RESOURCES_v1.Models;
 
 namespace HUMAN_RESOURCES_v1.Controllers
 {
-    public class VACACIONESController : Controller
+    public class VACACIONEsController : Controller
     {
         private HUMAN_RESOURCES_Entities db = new HUMAN_RESOURCES_Entities();
 
-        
+        // GET: VACACIONEs
         public ActionResult Index()
         {
             var vACACIONES = db.VACACIONES.Include(v => v.EMPLEADO);
             return View(vACACIONES.ToList());
         }
 
-      
+        // GET: VACACIONEs/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
@@ -36,14 +36,16 @@ namespace HUMAN_RESOURCES_v1.Controllers
             return View(vACACIONE);
         }
 
-        
+        // GET: VACACIONEs/Create
         public ActionResult Create()
         {
             ViewBag.id_empleado = new SelectList(db.EMPLEADOS, "id_empleado", "nombre");
             return View();
         }
 
-        
+        // POST: VACACIONEs/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "id_vacaciones,id_empleado,fecha_inicio_vacaciones,fecha_fin_vaciones,comentario_vacaiones")] VACACIONE vACACIONE)
@@ -59,7 +61,7 @@ namespace HUMAN_RESOURCES_v1.Controllers
             return View(vACACIONE);
         }
 
-        
+        // GET: VACACIONEs/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -75,7 +77,9 @@ namespace HUMAN_RESOURCES_v1.Controllers
             return View(vACACIONE);
         }
 
-      
+        // POST: VACACIONEs/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "id_vacaciones,id_empleado,fecha_inicio_vacaciones,fecha_fin_vaciones,comentario_vacaiones")] VACACIONE vACACIONE)
@@ -90,7 +94,7 @@ namespace HUMAN_RESOURCES_v1.Controllers
             return View(vACACIONE);
         }
 
-        
+        // GET: VACACIONEs/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
@@ -105,7 +109,7 @@ namespace HUMAN_RESOURCES_v1.Controllers
             return View(vACACIONE);
         }
 
-        
+        // POST: VACACIONEs/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
